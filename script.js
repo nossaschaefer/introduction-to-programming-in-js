a0716cd50459a733be073cb2637b367dba2d2ffc
Nossa Schaefer <nossa.schaefer@gmail.com>
2024-09-14 14:48:03 +0200

    Coding-Task: If statements

--- script.js
+++ script.js
@@ -85,3 +85,84 @@ console.log(isString(""));
 
 console.log(isString("John" + "Doe"));
 // result should be true
+
+//01) Spot the errors and fix them
+
+const size = 25;
+let result;
+
+if (size > 10 && size < 20) {
+  result = "Greater than 10";
+} else if (size > 20) {
+  result = "Greater than 20";
+} else {
+  result = "Lower than 10";
+}
+
+console.log(result);
+// Returns Greater than 10 but should return Greater than 20
+
+// 02) oddEven function
+// Implement an oddEven function.
+
+//  The function has one parameter which is a number
+//  If the number is an even number, the function returns the string even
+//  If the number is an od number, the function returns the string odd
+// TODO: Implement the oddEven function
+
+console.log(oddEven(4));
+// result should be even
+
+console.log(oddEven(3));
+// result should be odd
+
+console.log(oddEven(-1));
+// result should be odd
+
+console.log(oddEven(10));
+// result should be even
+
+function oddEven(number) {
+  if (number % 2 === 0) {
+    return "even";
+  } else {
+    return "odd";
+  }
+}
+
+// 03) oldYoung function
+// Implement a oldYoung function.
+
+//  The function has one parameter which is a number
+//  If the number is less than 16, return "children"
+//  If the number is less than 50, return "young person"
+//  Otherwise return "elder person"
+//  If the number is not a number or a negative number a string invalid parameter
+// TODO: Implement the oldYoung function
+
+console.log(oldYoung(27));
+// result should be young person
+
+console.log(oldYoung(6));
+// result should be children
+
+console.log(oldYoung(-1));
+// result should be invalid parameter
+
+console.log(oldYoung(86));
+// result should be elder person
+
+console.log(oldYoung("hi"));
+// result should be elder person
+
+function oldYoung(number) {
+  if (number < 0 || typeof number !== "number") {
+    return "invalid parameter";
+  } else if (number < 16) {
+    return "children";
+  } else if (number < 50) {
+    return "young person";
+  } else {
+    return "elder person";
+  }
+}
