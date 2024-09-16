cf71d84fa96071148c60ff2cf767ba249e1d8b69
Nossa Schaefer <nossa.schaefer@gmail.com>
2024-09-16 14:07:24 +0200

    chore: using loops

--- script.js
+++ script.js
@@ -166,3 +166,66 @@ function oldYoung(number) {
     return "elder person";
   }
 }
+
+console.log("oddNumbersFunction");
+// 01) oddNumbers function
+// Implement a oddNumbers function.
+
+//  The function has two parameters, both are numbers.
+//  Iterate over all numbers between first number and the second number.
+//  Store all oddNumbers in string, seperated by a comma and return the string.
+//  Allowed are only positive numbers, add a check.
+// TODO: Implement the oddNumbers function
+
+console.log(oddNumbers(0, 4));
+// result should be: 1,3
+
+console.log(oddNumbers(10, 33));
+// result should be: 11,13,15,17,19,21,23,25,27,29,31,33
+
+console.log(oddNumbers(9, 12));
+// result should be: 9,11
+
+function oddNumbers(num1, num2) {
+  for (let i = ++num1; i < num2; i++) {
+    if (i % 2 !== 0 && i > 0) {
+      console.log(i);
+    }
+  }
+}
+
+console.log(oddNumbers(0, 4));
+
+console.log("charCount function");
+// 02) charCount function
+// Implement a charCount function.
+
+//  The function has a first parameter for a word which is a string.
+//  The function has a second parameter which contains the character to search for which is a string.
+//  Check if the second parameter consists of only one character, if not, quit the function.
+//  The function returns the number of times the character from the second parameter is present in the word.
+//  The function should be case insensitive.
+// TODO: Implement the charCount function
+
+function charCount(word, char) {
+  let count = 0;
+  if (char.length !== 1) {
+    return;
+  } else {
+    for (let i = 0; i < word.length; i++) {
+      if (word[i] === char) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
+
+console.log(charCount("hello", "l"));
+// result should be: 2
+
+console.log(charCount("mama", "m"));
+// result should be: 2
+
+console.log(charCount("Resümee", "e"));
+// result should be: 3
