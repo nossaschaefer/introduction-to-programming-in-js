607a69624e6602c4db2b1c65cdb0c59de3d6e49b
Nossa Schaefer <nossa.schaefer@gmail.com>
2024-09-16 16:16:56 +0200

    chore: arrays

--- script.js
+++ script.js
@@ -229,3 +229,34 @@ console.log(charCount("mama", "m"));
 
 console.log(charCount("Resümee", "e"));
 // result should be: 3
+
+const coolColors = ["hotpink", "doderblue", "peachpuff"];
+coolColors.splice(1, 2, "indigo");
+console.log(coolColors);
+
+console.log("Arrays");
+// 06) - arrays
+// Prepare
+//  Create a new folder and prepare an index.html file that executes a script.js files.
+// 01) removeItem function
+// Implement a removeItem function.
+
+//  The first parameter is an array.
+//  The second parameter is a number. It's the non-zero-based index of the item that should get deleted.
+//  Remove the item and return an array.
+//  Ensure that the original array was not mutated.
+// TODO: Implement the removeItem function
+
+const animals = ["Dog", "Cat", "Lion"];
+console.log(removeItem(animals, 1));
+// result should be: ["Cat", "Lion"]
+
+console.log(animals);
+// result should be still: ["Dog", "Cat", "Lion"]
+
+const fruits = ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"];
+console.log(removeItem(fruits, 3));
+// result should be: ["Watermelon", "Banana", "Kiwi", "Pineapple", "Apple"]
+
+console.log(fruits);
+// result should be still: ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"]
---
+++ script2.js
@@ -0,0 +1,64 @@
+// 06) - arrays
+// 01) removeItem function
+// Implement a removeItem function.
+
+//  The first parameter is an array.
+//  The second parameter is a number. It's the non-zero-based index of the item that should get deleted.
+//  Remove the item and return an array.
+//  Ensure that the original array was not mutated.
+// TODO: Implement the removeItem function
+
+function removeItem(array, num) {
+  const arrayCopy = structuredClone(array);
+  arrayCopy.splice(--num, 1);
+  return arrayCopy;
+}
+
+const animals = ["Dog", "Cat", "Lion"];
+console.log(removeItem(animals, 1));
+// result should be: ["Cat", "Lion"]
+
+console.log(animals);
+// result should be still: ["Dog", "Cat", "Lion"]
+
+const fruits = ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"];
+console.log(removeItem(fruits, 3));
+// result should be: ["Watermelon", "Banana", "Kiwi", "Pineapple", "Apple"]
+
+console.log(fruits);
+// result should be still: ["Watermelon", "Banana", "Cherry", "Kiwi", "Pineapple", "Apple"]
+
+console.log("(02) sumOfCharacters function");
+// Implement a sumOfCharacters function.
+
+//  The function has one parameter, which is an array.
+//  Check the type of each array entry. If it's a string then store the count of characters.
+//  The function returns the total sum of all characters.
+// TODO: Implement the sumOfCharacters function
+
+function sumOfCharacters(arr) {
+  let totalCharacters = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (typeof arr[i] === "string") {
+      totalCharacters += arr[i].length;
+    }
+  }
+  return totalCharacters;
+}
+
+const arr = ["Hello", "1", " "];
+console.log(sumOfCharacters(arr));
+
+const arr1 = ["Luke", "Anakin", true, "Obi Wan", 333];
+console.log(sumOfCharacters(arr1));
+// result should be: 17
+
+const arr2 = [
+  "Code is",
+  "like humor",
+  ".",
+  "When you have",
+  "to explain it, it's bad!",
+];
+console.log(sumOfCharacters(arr2));
+// result should be: 55
